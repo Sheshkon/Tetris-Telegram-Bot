5f83cccba2e8dc850c02fd5fdab1844a5ac4a375
Alexei <sheskoalexei@gmail.com>
2022-02-24 16:05:02 +0300

    add catching exception

--- rsa_decrypt.py
+++ rsa_decrypt.py
@@ -5,9 +5,14 @@ from config import PRIVATE_KEY
 
 
 async def decrypt(encrypted_data: str):
-    key = RSA.importKey(PRIVATE_KEY)
-    key = PKCS1_OAEP.new(key)
-    decoded_data = base64.b64decode(encrypted_data)
-    decrypted_text = key.decrypt(decoded_data).decode('utf-8')
+    decrypted_text = ''
+    try:
+        key = RSA.importKey(PRIVATE_KEY)
+        key = PKCS1_OAEP.new(key)
+        decoded_data = base64.b64decode(encrypted_data)
+        decrypted_text = key.decrypt(decoded_data).decode('utf-8')
+    except:
+        decrypted_text = 'decryption error'
+
     print(decrypted_text)
     return decrypted_text
